c5f65e3ca675d4ac0522f6cf3e3138002ba30bf9
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-06-12 12:35:43 -0400

    worked on put requests in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,20 +42,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body, {
-   where: {
-    id: req.params.id,
-   },
-  })
-    .then((category) => {
-      // find all associated tags from Category
-      return Category.findAll({ where: { category_id: req.params.id } });
-    })
-      .then((categories) => {
-        // list of current category ids
-        const categoryIds = categories.map(({ }))
-      })
-  )
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category found with that id.' });
+      return
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
