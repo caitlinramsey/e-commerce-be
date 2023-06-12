1c24e1b47c0a1ae5fdebd52587ce725ed810c80e
Caitlin Ramsey <caitlinramsey@Caitlins-MacBook-Pro.local>
2023-06-12 11:29:47 -0400

    worked on get requests in category-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,19 +6,39 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+    // products??
+  });
+  
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  ).then((categoryData) => {
+    res.json(categoryData);
+  }); //add product data??
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
